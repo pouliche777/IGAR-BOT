e99850bb649bb0b4d0b7e5152fc02d681ab50bb3
Samuel Lord <sam.lord@hotmail.ca>
2023-07-09 13:51:10 -0400

    CA MARCHE!!!

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,8 +1,8 @@
 
 use std::env;
 use reqwest;
-use regex::Regex;
-use serde::{Deserialize};
+
+use serde::{Deserialize, Serialize};
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready},
@@ -118,123 +118,213 @@ async fn get_data(access_token: &str, code: &str) -> Result<(), Box<dyn std::err
 }
 
 async fn parse_data(data: serde_json::Value) -> Result<(), Box<dyn std::error::Error>> {
-    #[derive(Debug, Deserialize)]
-struct Report {
-    data: ReportData,
-}
-
-#[derive(Debug, Deserialize)]
-struct ReportData {
-    reportData: ReportInfo,
-}
-
-#[derive(Debug, Deserialize)]
-struct ReportInfo {
-    report: Rankings,
-}
-
-#[derive(Debug, Deserialize)]
-struct Rankings {
-    data: Vec<Ranking>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Ranking {
-    affixes: Vec<u32>,
-    bracket: u32,
-    bracketData: u32,
-    computedScore: f64,
-    damageTakenExcludingTanks: u32,
-    deaths: u32,
-    difficulty: u32,
-    duration: u32,
-    encounter: Encounter,
-    fightID: u32,
-    kill: u32,
-    medal: String,
-    partition: u32,
-    roles: Roles,
-    score: Score,
-    size: u32,
-    speed: Speed,
-    team: Vec<TeamMember>,
-    zone: u32,
-}
-
-#[derive(Debug, Deserialize)]
-struct Encounter {
-    id: u32,
-    name: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Roles {
-    dps: RoleInfo,
-    healers: RoleInfo,
-    tanks: RoleInfo,
-}
-
-#[derive(Debug, Deserialize)]
-struct RoleInfo {
-    characters: Vec<Character>,
-    name: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Character {
-    amount: f64,
-    best: String,
-    bracket: u32,
-    bracketData: u32,
-    bracketPercent: u32,
-    class: String,
-    id: u32,
-    name: String,
-    rank: String,
-    rankPercent: u32,
-    server: Server,
-    spec: String,
-    totalParses: u32,
-}
-
-#[derive(Debug, Deserialize)]
-struct Server {
-    id: u32,
-    name: String,
-    region: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Score {
-    best: String,
-    rank: String,
-    rankPercent: u32,
-    totalParses: u32,
-}
-
-#[derive(Debug, Deserialize)]
-struct Speed {
-    best: String,
-    rank: String,
-    rankPercent: u32,
-    totalParses: u32,
-}
-
-#[derive(Debug, Deserialize)]
-struct TeamMember {
-    class: String,
-    id: u32,
-    name: String,
-    role: String,
-    spec: String,
-}
-
-    println!("{:?}", data);
-    let report_data: Result<ReportData, serde_json::Error> = serde_json::from_value(data);
-    match report_data {
-        Ok(report_data) => println!("{:?}", report_data),
-        Err(error) => println!("Deserialization error: {}", error),
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Root {
+        pub data: Data,
     }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Data {
+        pub report_data: ReportData,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct ReportData {
+        pub report: Report,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Report {
+        pub rankings: Rankings,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Rankings {
+        pub data: Vec<Daum>,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Daum {
+        #[serde(rename = "fightID")]
+        pub fight_id: i64,
+        pub partition: i64,
+        pub zone: i64,
+        pub encounter: Encounter,
+        pub difficulty: i64,
+        pub size: i64,
+        pub kill: i64,
+        pub duration: i64,
+        pub bracket_data: f64,
+        pub deaths: i64,
+        pub damage_taken_excluding_tanks: i64,
+        pub roles: Roles,
+        pub bracket: i64,
+        pub guild: Guild,
+        pub speed: Speed,
+        pub execution: Execution,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Encounter {
+        pub id: i64,
+        pub name: String,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Roles {
+        pub tanks: Tanks,
+        pub healers: Healers,
+        pub dps: Dps,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Tanks {
+        pub name: String,
+        pub characters: Vec<Character>,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Character {
+        pub id: i64,
+        pub name: String,
+        pub server: Server,
+        pub class: String,
+        pub spec: String,
+        pub amount: f64,
+        pub bracket_data: i64,
+        pub bracket: i64,
+        pub rank: String,
+        pub best: String,
+        pub total_parses: i64,
+        pub bracket_percent: i64,
+        pub rank_percent: i64,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Server {
+        pub id: i64,
+        pub name: String,
+        pub region: String,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Healers {
+        pub name: String,
+        pub characters: Vec<Character2>,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Character2 {
+        pub id: i64,
+        pub name: String,
+        pub server: Server2,
+        pub class: String,
+        pub spec: String,
+        pub amount: f64,
+        pub bracket_data: i64,
+        pub bracket: i64,
+        pub rank: String,
+        pub best: String,
+        pub total_parses: i64,
+        pub bracket_percent: i64,
+        pub rank_percent: i64,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Server2 {
+        pub id: i64,
+        pub name: String,
+        pub region: String,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Dps {
+        pub name: String,
+        pub characters: Vec<Character3>,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Character3 {
+        pub id: i64,
+        pub name: String,
+        pub server: Server3,
+        pub class: String,
+        pub spec: String,
+        pub amount: f64,
+        pub bracket_data: i64,
+        pub bracket: i64,
+        pub rank: String,
+        pub best: String,
+        pub total_parses: i64,
+        pub bracket_percent: i64,
+        pub rank_percent: i64,
+        pub exploit: Option<i64>,
+        pub banned: Option<bool>,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Server3 {
+        pub id: i64,
+        pub name: String,
+        pub region: String,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Guild {
+        pub id: i64,
+        pub name: String,
+        pub faction: i64,
+        pub server: Server4,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Server4 {
+        pub id: i64,
+        pub name: String,
+        pub region: String,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Speed {
+        pub rank: String,
+        pub best: String,
+        pub total_parses: i64,
+        pub rank_percent: i64,
+    }
+    
+    #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Execution {
+        pub rank: String,
+        pub best: String,
+        pub total_parses: i64,
+        pub rank_percent: i64,
+    }
+    let root: Root = serde_json::from_value(data)?;
+    println!("{:#?}", root);
+    
+    
 
     Ok(())
 }
